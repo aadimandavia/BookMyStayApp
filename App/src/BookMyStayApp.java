c7fa19f43d0b4d2ec48471b293a04cf6e45a7b8e
aadimandavia <aadim2612@gmail.com>
2026-03-16 03:17:24 +0530

    UC1 Added

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class BookMyStayApp {
+    public static void main(String[] args) {
+       System.out.println("Welcome to Hotel bookkking Management System");
+       System.out.println("System Initilized Succesfully");
+    }
+}
